00817e1e5d57f26c5c519245d4bfa6f0b65f3369
schang1146 <sammychang2185@gmail.com>
2020-03-20 18:56:35 -0700

    finished ex2 in hashtables; previous commit only finished ex1

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -14,10 +14,22 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
-    route = [None] * length
+    route = [None] * (length - 1)
 
     """
     YOUR CODE HERE
     """
+    # iterate through `tickets` array and store `ticket` such that:
+    # key = source (ticket[0])
+    # value = destination (ticket[1])
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
 
-    pass
+    # populate route by appending from value of hashtable starting with key: "NONE"
+    for idx in range(length-1):
+        if idx == 0:
+            route[idx] = hash_table_retrieve(hashtable, "NONE")
+        else:
+            route[idx] = hash_table_retrieve(hashtable, route[idx-1])
+
+    return route
