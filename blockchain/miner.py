2ade2a2cc60708dac733b8c278f3c6ab2b436911
schang1146 <sammychang2185@gmail.com>
2020-03-20 20:20:12 -0700

    mined some coins!

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -25,12 +25,14 @@ def proof_of_work(last_proof):
     print("Searching for next proof")
     proof = 0
     #  TODO: Your code here
+    while valid_proof(last_proof, proof) is False:
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
 
 
-def valid_proof(last_hash, proof):
+def valid_proof(last_proof, proof):
     """
     Validates the Proof:  Multi-ouroborus:  Do the last six characters of
     the hash of the last proof match the first six characters of the hash
@@ -40,7 +42,7 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    return hashlib.sha256(f'{last_proof}'.encode()).hexdigest()[-6:] == hashlib.sha256(f'{proof}'.encode()).hexdigest()[:6]
 
 
 if __name__ == '__main__':
